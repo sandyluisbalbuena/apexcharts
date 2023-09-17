5b3af96a78da0912c756c07f9b5ce2fded5273d2
sandyluisbalbuena <balbuenasandyluis12@gmail.com>
2023-09-17 14:27:38 +0800

    initial

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -7,8 +7,8 @@ import axios from 'axios';
 export default function Home() {
 
 	const fetchingCoinsMarkets = async () => {
-		const { data:fetchedCoinsMarkets } = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en`);
-		// const { data:fetchedCoinsMarkets } = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en`);
+		// const { data:fetchedCoinsMarkets } = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en`);
+		const { data:fetchedCoinsMarkets } = await axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=true&price_change_percentage=1h%2C24h%2C7d&locale=en`);
 		return fetchedCoinsMarkets;
 	}
 
